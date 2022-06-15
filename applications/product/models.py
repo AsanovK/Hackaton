097c4c86dca699efb4e7ef0625ae9c0b4b53e10c
adeena911 <adinadavletova@gmail.com>
2022-06-15 21:04:50 +0600

    create product

--- applications/product/models.py
+++ applications/product/models.py
@@ -6,22 +6,20 @@ User = get_user_model()
 
 
 class Product(models.Model):
-    title = models.Charfield(max_length=80, verbose_name='Title')
-    description = models.Textfield(verbose_name='Description')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='Product')
+    title = models.CharField(max_length=80, verbose_name='Title')
+    description = models.TextField(verbose_name='Description')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Price')
+    # category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='Product')
     in_stock = models.BooleanField(default=True)
     quantity = models.PositiveIntegerField(default=1)
 
-
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
 
 
 class ProductImage(models.Model):
-    image = models.ImageField(upload='images')
+    image = models.ImageField(upload_to='images')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='image')
-
 
     def __str__(self):
         return self.product.title
